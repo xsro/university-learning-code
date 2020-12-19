166d5102e6a81d387d5531f2fc94ba612711e7bd
lc@kun'sASUS <xsro@foxmail.com>
2020-12-19 13:36:35 +0800

    自控实验3 增加系统辨识的函数

--- matlab/自动控制原理/3频域实验/main.m
+++ matlab/自动控制原理/3频域实验/main.m
@@ -1,5 +1,37 @@
+np=[3 2];
+nz=[0 1];
+tit={'俯仰通道实验','偏航通道实验'};
 [~,sheets] = xlsfinfo('data.xlsx');
-[F,A,P]=getdata(1);
-[F2,A2,P2]=getdata(2);
-ident
+N=2;
+for i=1:N
+    [F,A,P]=getdata(i);
+    h=A.*exp(1i*P/180*pi);
+    data=idfrd(h,F,0);
+    model=tfest(data,np(i),nz(i));
+    display(model)
+    figure(1);
+    subplot(1,2,i)
+    bode(data,'rx-',model);
+    legend('实测值','系统识别值');
+    title(tit(i));
+    grid on
+    %绘制阶跃响应信号来判断是否稳定
+    
+    figure(2);
+    subplot(1,2,i);
+    stepplot(model);
+    
+    figure (3)
+    G=model/(1-model);
+    G=minreal(G);
+    subplot(2,2,i);
+    pzmap(G);
+    title(tit(i));
+    subplot(2,2,N+i);
+     nyquist(G);
+      
+end
+
+
+
 
