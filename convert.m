018fd76cf064470f7cd96ed3e5ef6b697289cded
Chen Liu <xsro@foxmail.com>
2021-06-11 23:55:57 +0800

    :art: index.html

--- convert.m
+++ convert.m
@@ -1,6 +1,4 @@
 function convert(dst)
-    %Convert Matlab live script to html.follow the instruction from:
-    %https://www.mathworks.com/matlabcentral/answers/282820-programmatically-run-and-export-live-script#comment_1457761
     dirInfo = dir('**/*.mlx');
     N=length(dirInfo);
     fprintf("[start]convert %d mlx to %s\n",N,dst);
@@ -18,6 +16,8 @@ function convert(dst)
         fileout = strcat(folder,'/',name);
         htmls(i)=fileout;
         fileout=convertStringsToChars(fileout);
+        %Convert Matlab live script to html.follow the instruction from:
+        %https://www.mathworks.com/matlabcentral/answers/282820-programmatically-run-and-export-live-script#comment_1457761
         matlab.internal.liveeditor.openAndConvert(mlxloc,fileout);
         fprintf("%d:converted %s\n",i,mlxloc);
     end
@@ -26,11 +26,21 @@ function convert(dst)
     indexFile=strcat(dst,'/index.html');
     fid=fopen(indexFile,'w');
     head='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
-    fprintf(fid,"<head>\n%s\n</head>\n<body>\n<ol>",head);
+    fprintf(fid,"<head>\n%s\n</head>\n<body><h1>my MATLAB Live Script Collection</h1>\n",head);
+    subtitle="";
     for i=1:length(htmls)
-        ref=strrep(htmls(i),strcat(pwd,'/',dst,'/'),"");
+        html=htmls(i);
+        ref=strrep(html,strcat(pwd,'/',dst,'/'),"");
         url='./'+ref;
-        fprintf(fid,"<li><a href='%s'>%s</a></li>\n",url,ref);
+        segs=split(ref,'/');
+        if segs(1)~=subtitle
+            if i~=1
+                fprintf(fid,"</ol>\n");
+            end
+            fprintf(fid,"<h2>%s</h2>\n<ol>\n",segs(1));
+            subtitle=segs(1);
+        end
+        fprintf(fid,"<li><a href='%s'>%s</a></li>\n",url,join(segs(2:end),'/'));
     end
     fprintf(fid,"%s",'</ol>');
     fprintf(fid,"%s",'</body>');
